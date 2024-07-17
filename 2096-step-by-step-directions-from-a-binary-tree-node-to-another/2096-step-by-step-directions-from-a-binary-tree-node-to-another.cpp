8eced3089508249bf560625e8da21ee8951a7a64
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-07-17 20:39:16 +0530

    Time: 137 ms (95.27%), Space: 119.1 MB (47.18%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -10,28 +10,62 @@
  * };
  */
 class Solution {
-private:
-    void  ()
+    TreeNode*solve(TreeNode* root, int startValue, int destValue){
+        if(root==NULL){
+            return NULL;
+        }
+        if(root->val ==  startValue || root->val==destValue){
+            return root ; 
+        }
+        TreeNode* left=solve(root->left,startValue,destValue);
+        TreeNode* right=solve(root->right,startValue,destValue);
+
+        if(left!=NULL && right!=NULL){
+            return root;
+        }
+        else if(left!=NULL && right ==NULL){
+            return left;
+        }
+        else if(left==NULL && right !=NULL){
+            return  right;
+        }
+        else{
+            return NULL;
+
+        }
+    }
+    bool path(TreeNode* root, int destValue,string &str){
+        if(root==NULL){
+            return 0;
+        }
+        if(root->val==destValue){
+            return 1;
+        }
+        str.push_back('L');
+        if(path(root->left,destValue,str)){
+            return 1;
+        }
+        str.pop_back();
+        str.push_back('R');
+        if(path(root->right,destValue,str)){
+            return 1;
+        }
+            str.pop_back();
+
+    
+        return 0;
+    }
 public:
-    string getDirections(TreeNode* root, int startValue, int destValue) {
-      map<  TreeNode*,TreeNode*>mp;
-      mp[root]=NULL;
-      queue<TreeNode*>q;
-      q.push(root);
-      while(!q.empty()){
-        TreeNode*rr=q.front();
-        q.pop();
-        if(rr->left!=NULL){
-            mp[rr->left]=rr;
-            q.push(rr->left);
-        }
-        if(rr->right!=NULL){
-            mp[rr->right]=rr;
-            q.push(rr->right);
-
-        }
-      }
-
-
+    string getDirections(TreeNode* root, int startValue, int destValue) 
+    {
+        TreeNode* ance = solve(root,  startValue,  destValue);
+        string str;
+        bool i=path(ance,startValue,str);
+        string des="";
+        bool h=path(ance,destValue,des);
+        string add=string(str.size(),'U');
+        string ans =add +des;
+        return ans;
+        
     }
 };

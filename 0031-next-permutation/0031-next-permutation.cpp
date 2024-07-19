5b7d654055d5ee58565a8d8393f85c38b13f25c3
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-07-19 09:01:12 +0530

    Time: 0 ms (100%), Space: 14.7 MB (24.14%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,18 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int max_index=0;
-        for(int i=1;i<nums.size();i++){
-            if(nums[max_index]<nums[i]){
-                max_index=i;
-            }
-        }
-        if(max_index==0){
-            swap(nums[max_index],nums[nums.size()-1]);
-        }
-        else{
-            swap(nums[max_index],nums[max_index-1]);
-        }
-        return;
+        next_permutation(nums.begin(),nums.end());
+        
     }
 };

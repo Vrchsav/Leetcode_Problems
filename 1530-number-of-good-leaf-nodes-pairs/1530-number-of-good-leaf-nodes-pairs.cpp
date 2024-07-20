c1bdfce9c158be08c4222982d1164a3a9828d214
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-07-20 18:32:46 +0530

    Time: 44 ms (74.02%), Space: 34.9 MB (57.44%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.cpp
@@ -10,29 +10,42 @@
  * };
  */
 class Solution {
-    public:
-    int solve(TreeNode* root, int distance, int length, int &ans) {
-        if (root == NULL) {
-            return 0;
+    vector<int>solve (TreeNode* root,int distance,int &ans){
+        if(root==NULL){
+            return {0};
         }
-        
-        if (root->left == NULL && root->right == NULL) {
-            return length;
+        if(root->left==NULL && root->right==NULL){
+            return {1};
         }
-        
-        int left = solve(root->left, distance, length + 1, ans);
-        int right = solve(root->right, distance, length + 1, ans);
-        
-        if (left != 0 && right != 0 && left + right - 2 * length <= distance) {
-            ans++;
+
+        vector<int>left=solve(root->left,distance,ans);
+        vector<int>right=solve(root->right,distance,ans);
+        for(int i=0;i<left.size();i++){
+            for(int j=0;j<right.size();j++){
+                if((left[i]!=0 && right[j]!=0) && left[i]+right[j]<=distance){
+                    ans++;
+                }
+            }
         }
-        
-        return max(left, right);
+        vector<int>am;
+        for(int i=0;i<left.size();i++){
+            int a=left[i]+1;
+            if(left[i]!=0 && a<=distance)
+            am.push_back(a);
+        }
+        for(int j=0;j<right.size();j++){
+                int a=right[j]+1;
+                if(right[j]!=0 && a<=distance)
+
+                am.push_back(a);
+            }
+        return am;
+
     }
-    
+public:
     int countPairs(TreeNode* root, int distance) {
-        int ans = 0;
-        solve(root, distance, 0, ans);
+        int ans=0;
+        vector<int>aa=solve(root,distance,ans);
         return ans;
     }
 };

52659faf12e277c956e8875cd8031e1cdc1abd07
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-10-10 12:23:42 +0530

    Time: 0 ms (100%), Space: 14 MB (99.38%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -10,37 +10,28 @@
  * };
  */
 class Solution {
+private:
+    void solve(TreeNode*root,int level,vector<int>&ans){
+        if(root==NULL){
+            return;
+        }
+        if(level==ans.size()){
+            ans.push_back(root->val);
+        }
+        else{
+            ans[level]=root->val;
+        }
+        solve(root->left,level+1,ans);
+        solve(root->right,level+1,ans);
+
+    }
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
         if(root==NULL){
             return ans;
         }
-        map<int,int> top_n;
-        queue<pair<TreeNode*,int>>q;
-        q.push(make_pair(root,0));
-        while(!q.empty()){
-            pair<TreeNode*,int>temp=q.front();
-            q.pop();
-            TreeNode* front_n=temp.first;
-            int level=temp.second;
-           
-
-           
-                top_n[level]=front_n->val;
-            
-
-            if(front_n->left){
-                q.push(make_pair(front_n->left,level+1));
-            }
-            if(front_n->right){
-                q.push(make_pair(front_n->right,level+1));
-            }
-            }
-        
-        for (auto i : top_n) {
-            ans.push_back(i.second);
-        }
+        solve(root,0,ans);
         return ans;
     }
 };

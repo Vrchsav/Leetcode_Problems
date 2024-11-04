8fffe128094a420754c2dbd24ff6d0f65d310954
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-11-04 12:01:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -5,29 +5,36 @@ class Solution {
         queue<pair<int, int>> q;
         q.push({i, j});
         visited[i][j] = 1;
-        
-        // Define the four possible directions: right, left, down, up
-        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-        
         while (!q.empty()) {
             pair<int, int> top = q.front();
             q.pop();
             
-            for (auto dir : directions) {
-                int new_i = top.first + dir.first;
-                int new_j = top.second + dir.second;
-                
-                // Check if the new cell is within bounds, is land ('1'), and is not visited
-                if (new_i >= 0 && new_i < n && new_j >= 0 && new_j < m && 
-                    grid[new_i][new_j] == '1' && visited[new_i][new_j] == 0) {
-                    
-                    q.push({new_i, new_j});
-                    visited[new_i][new_j] = 1;
-                }
+            // Check right
+            if (top.second + 1 < m && grid[top.first][top.second + 1] == '1' && visited[top.first][top.second + 1] == 0) {
+                q.push({i, top.second + 1});
+                visited[i][top.second + 1] = 1;
+            }
+            
+            // Check up
+            if (top.first - 1 >= 0 && grid[top.first - 1][top.second] == '1' && visited[top.first - 1][top.second] == 0) {
+                q.push({top.first - 1, top.second});
+                visited[top.first - 1][top.second] = 1;
+            }
+            
+            // Check down
+            if (top.first + 1 < n && grid[top.first + 1][top.second] == '1' && visited[top.first + 1][top.second] == 0) {
+                q.push({top.first + 1, top.second});
+                visited[top.first + 1][top.second] = 1;
+            }
+            
+            // Check left
+            if (top.second - 1 >= 0 && grid[top.first][top.second - 1] == '1' && visited[top.first][top.second - 1] == 0) {
+                q.push({top.first, top.second - 1});
+                visited[top.first][top.second - 1] = 1;
             }
         }
+        return;
     }
-    
 public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();

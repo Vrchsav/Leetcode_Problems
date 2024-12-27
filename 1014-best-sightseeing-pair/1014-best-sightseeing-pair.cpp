6710b49e28853ad565614442562a94fe9c5fcc5b
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2024-12-28 02:12:34 +0530

    Time: 3 ms (29.09%), Space: 58 MB (5.1%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -1,24 +1,27 @@
 class Solution {
-    int solve(vector<int>values,int count,int index){
+    int solve(vector<int>&values,int count,int index,vector<vector<int>>&mpp){
         if(count ==0  || index<0){
             return 0;
         }
-        
+        if(mpp[count][index]!=-1){
+            return mpp[count][index];
+        }
         int inc =0;
         if(count==1){
-            inc=values[index]+index+solve(values,count-1,index-1);
+            inc=values[index]+index+solve(values,count-1,index-1,mpp);
         }
         if(count==2){
-            inc=values[index]-index+solve(values,count-1,index-1);
+            inc=values[index]-index+solve(values,count-1,index-1,mpp);
 
         }
-        int not_inc=solve(values,count,index-1);
-        return max(inc,not_inc);
+        int not_inc=solve(values,count,index-1,mpp);
+        return mpp[count][index] =max(inc,not_inc);
     }
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int n=values.size();
-        int ans=solve(values,2,n-1);
+        vector<vector<int>>mpp(3,vector<int>(n,-1));
+        int ans=solve(values,2,n-1,mpp);
         return ans;
     }
 };

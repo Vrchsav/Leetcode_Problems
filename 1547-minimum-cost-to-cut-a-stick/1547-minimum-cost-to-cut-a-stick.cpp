b4ca823aedf621677b77d92f2b0421fb3550ac58
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-01-03 19:41:38 +0530

    Time: 15 ms (93.26%), Space: 15.5 MB (6.53%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,36 +1,36 @@
 class Solution {
-     const int MOD=1e9+7;
+    const int MOD = 1e9 + 7;
 
-    
-    int solve(int i, int j, vector<int>& cuts,vector<vector<int>>&dp) {
-        if (i > j) {
-            return 0;
+    int solve( int m, vector<int>& cuts ) {
+
+        vector<vector<int>> dp(m + 2, vector<int>(m + 2, 0));
+
+        for (int i = m; i > 0; i--) {
+            for (int j =  1; j <= m; j++) {
+                if(i>j){
+                    continue;
+                }
+                int ans = INT_MAX;
+
+                for (int k = i; k <= j; k++) {
+                    int cost = cuts[j + 1] - cuts[i - 1] +dp[i][k-1]+dp[k+1][j];
+                               
+                    ans = min(ans, cost % MOD);
+                }
+
+             dp[i][j] = ans % MOD;
+            }
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        int ans = INT_MAX;
-        
-        for (int k = i; k <= j; k++) {
-            int cost = cuts[j + 1] - cuts[i - 1] 
-                     + solve(i, k - 1, cuts,dp) 
-                     + solve(k + 1, j, cuts,dp);
-            ans = min(ans, cost % MOD);
-        }
-        
-        return dp[i][j] =ans % MOD;
+        return dp[1][m];
     }
 
 public:
-    int minCost(int n, vector<int> &cuts) {
-        int m=cuts.size();
-        vector<vector<int>>dp(m+1,vector<int>(m+1,-1));
-        cuts.insert(cuts.begin(),0);
+    int minCost(int n, vector<int>& cuts) {
+        int m = cuts.size();
+        vector<vector<int>> dp(m + 1, vector<int>(m + 1, -1));
+        cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
-        sort(cuts.begin(),cuts.end());
-        return solve(1,m,cuts,dp);
-
-        
+        sort(cuts.begin(), cuts.end());
+        return solve( m, cuts);
     }
 };

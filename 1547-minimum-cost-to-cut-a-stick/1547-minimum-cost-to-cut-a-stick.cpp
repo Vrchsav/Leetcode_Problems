9d5cb4d337527bc9e5c67469cfc5b0bf3482aff1
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-01-03 19:41:01 +0530

    Time: 35 ms (46.44%), Space: 13.8 MB (48.74%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -2,30 +2,34 @@ class Solution {
      const int MOD=1e9+7;
 
     
-    int solve(int i, int j, vector<int>& cuts) {
+    int solve(int i, int j, vector<int>& cuts,vector<vector<int>>&dp) {
         if (i > j) {
             return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
         
         int ans = INT_MAX;
         
         for (int k = i; k <= j; k++) {
             int cost = cuts[j + 1] - cuts[i - 1] 
-                     + solve(i, k - 1, cuts) 
-                     + solve(k + 1, j, cuts);
+                     + solve(i, k - 1, cuts,dp) 
+                     + solve(k + 1, j, cuts,dp);
             ans = min(ans, cost % MOD);
         }
         
-        return ans % MOD;
+        return dp[i][j] =ans % MOD;
     }
 
 public:
     int minCost(int n, vector<int> &cuts) {
         int m=cuts.size();
+        vector<vector<int>>dp(m+1,vector<int>(m+1,-1));
         cuts.insert(cuts.begin(),0);
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
-        return solve(1,m,cuts);
+        return solve(1,m,cuts,dp);
 
         
     }

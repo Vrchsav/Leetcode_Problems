18d5f02b76d0bedc340b23b2c3836795e2e93683
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-01-05 20:47:04 +0530

    Time: 217 ms (13.31%), Space: 13.8 MB (49.35%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,24 +1,30 @@
 
 class Solution {
-    const int MOD=1e9+7;
-    int solve(vector<int>& nums,int i,int j){
-        if(i>j){
+    const int MOD = 1e9 + 7;
+    int solve(vector<int>& nums, int i, int j, vector<vector<int>> &dp) {
+        if (i > j) {
             return 0;
         }
-        int maxi=INT_MIN;
-        for(int k=i;k<=j;k++){
-            int a=nums[k]*nums[i-1]*nums[j+1]+solve(nums,i,k-1)+solve(nums,k+1,j);
-            maxi=max(maxi,a)%MOD;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        return maxi%MOD;
+        int maxi = INT_MIN;
+        for (int k = i; k <= j; k++) {
+            int a = nums[k] * nums[i - 1] * nums[j + 1] +
+                    solve(nums, i, k - 1,dp) + solve(nums, k + 1, j,dp);
+            maxi = max(maxi, a) % MOD;
+        }
+        return dp[i][j]=maxi % MOD;
     }
+
 public:
     int maxCoins(vector<int>& nums) {
-        int n=nums.size();
-        nums.insert(nums.begin(),1);
+        int n = nums.size();
+        nums.insert(nums.begin(), 1);
         nums.push_back(1);
-        int ans =solve(nums,1,n);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
+
+        int ans = solve(nums, 1, n,dp);
         return ans;
-        
     }
 };

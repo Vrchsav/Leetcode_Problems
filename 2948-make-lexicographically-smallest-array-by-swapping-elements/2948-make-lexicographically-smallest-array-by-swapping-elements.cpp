479e6106790f0cb074483fa8a624597248f6234a
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-01-25 20:17:15 +0530

    Time: 1679 ms (5.4%), Space: 200.2 MB (35.81%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -3,19 +3,40 @@ public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
         int n=nums.size();
         int i=0;
-        while(i<n){
-            bool flag=false;
-            for(int j=i;j<n;j++){
-                if(nums[i]>nums[j] && nums[i]-nums[j]<=limit){
-                    swap(nums[i],nums[j]);
-                    flag=true;
-                }
-            }
-            if(flag==false){
-                i++;
-            }
+        vector<int>sorted=nums;
+        sort(sorted.begin(),sorted.end());
+        map<int, vector<int>*> mpp;
+        vector<int>* gp = new vector<int>;  
+        gp->push_back(sorted[0]);
+        mpp[sorted[0]]=gp;
+        for(int i=1;i<nums.size();i++){
+            if(sorted[i]-sorted[i-1]<=limit){
+                gp->push_back(sorted[i]);
+                mpp[sorted[i]]=gp;
+            }else{
+                
+                gp = new vector<int>;  
+                gp->push_back(sorted[i]);
+                mpp[sorted[i]]=gp;
 
+
+            }
         }
+
+
+
+
+        for(int i=0;i<nums.size();i++){
+            vector<int>*temp=mpp[nums[i]];
+            swap(nums[i],(*temp)[0]);
+            if (temp->size() > 1) {    
+                temp->erase(temp->begin()); 
+            }
+        }
+
+
+
+        
         return nums;
         
     }

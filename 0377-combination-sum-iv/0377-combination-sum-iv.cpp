c7a8235bde67d6313203a0f00a0af9fdba8cd974
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-01-27 15:00:49 +0530

    Time: 4 ms (30.15%), Space: 10.3 MB (6.73%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,23 +1,28 @@
 class Solution {
-    int solve(vector<int>& nums,int target){
+    int solve(vector<int>& nums,int target, vector<vector<int>>&dp,int i){
         
         if (target==0){
             return 1;
         }
+        if(dp[i][target]!=-1){
+            return dp[i][target];
+        }
         int count=0;
-        for(auto a:nums ){
-            if(target>=a){
-                count+=solve(nums,target-a);
+        for(int j=0;j<nums.size();j++){
+            if(target>=nums[j]){
+                count+=solve(nums,target-nums[j],dp,j);
             }
         }
 
-        return count;
+        return dp[i][target] =count;
 
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
         int ans;
-        ans=solve(nums,target);
+        ans=solve(nums,target,dp,0);
         return ans;
 
         

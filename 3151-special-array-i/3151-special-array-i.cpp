18d41c905d9d5ad1dfcba8c8f176fc9699e6a0cd
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-02-04 00:58:48 +0530

    Time: 0 ms (100%), Space: 27.8 MB (22.36%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.cpp
+++ 3151-special-array-i/3151-special-array-i.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
+        if(nums.size()==2){
+            if((nums[0]%2==0 && (nums[1]%2==0) || (nums[0]%2!=0 && (nums[1]%2!=0 )))){
+return false;
+        }}
         for(int i=1;i<nums.size()-1;i++){
 
             if((nums[i]%2==0 && (nums[i-1]%2==0 || nums[i+1]%2==0)) || (nums[i]%2!=0 && (nums[i-1]%2!=0 || nums[i+1]%2!=0))  ){

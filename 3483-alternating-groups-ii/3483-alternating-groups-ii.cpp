c5305c0e9071fff90f637bc58ce15a9f366b9b77
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-03-10 15:17:43 +0530

    Time: 71 ms (59.92%), Space: 108.2 MB (24.68%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -2,20 +2,24 @@ class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
         int ans = 0;
+
+        for (int i = 0; i < k - 1; i++) {
+            colors.push_back(colors[i]);
+        }
         int siz = colors.size();
         int j = 0;
-
-        for (int i = 1; i < 2 * siz; i++) { // Simulate circular array for 2 cycles
-            if (colors[(i - 1) % siz] == colors[i % siz]) {
-                j = i; // Reset start of alternating sequence
+        int i;
+        for (i = 1; i < siz; i++) {
+            if (colors[i - 1] == colors[i]) {
+                j = i;
             }
-
-            if (i - j + 1 == k) { 
-                ans++; 
-                j = i; // Move past the found sequence
+            if (i - j + 1 == k) {
+                ans++;
+                j++;
+               
             }
         }
-
+        
         return ans;
     }
 };

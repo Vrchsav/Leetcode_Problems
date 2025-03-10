ad82d00762328b9559aaa9e3d04295b636a9f93b
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-03-10 15:17:58 +0530

    Time: 88 ms (9.7%), Space: 102.7 MB (77%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.cpp
@@ -3,14 +3,11 @@ public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
         int ans = 0;
 
-        for (int i = 0; i < k - 1; i++) {
-            colors.push_back(colors[i]);
-        }
         int siz = colors.size();
         int j = 0;
-        int i;
-        for (i = 1; i < siz; i++) {
-            if (colors[i - 1] == colors[i]) {
+        int i=1;
+        while (j<siz) {
+            if (colors[(i - 1)%siz] == colors[i%siz]) {
                 j = i;
             }
             if (i - j + 1 == k) {
@@ -18,6 +15,7 @@ public:
                 j++;
                
             }
+            i++;
         }
         
         return ans;

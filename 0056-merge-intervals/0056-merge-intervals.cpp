95515a725a9e66bc163957546f2fa26e5fb2b58a
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-03-26 05:08:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,36 +1,17 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        // Ensure intervals are sorted by start time for efficient merging
-        sort(intervals.begin(), intervals.end());
+    vector<vector<int>> merge(vector<vector<int>>& interval) {
+        vector<vector<int>> ans;
+        ans.push_back(interval[0]);
+        sort(interval.begin(), interval.end());
+        for(int i=1;i<interval.size();i++){
+            if(interval[i][0]<=ans.back()[1]){
+                ans.back()[1]=max(ans.back()[1],interval[i][1]);
+            }else{
+        ans.push_back(interval[i]);
 
-        vector<vector<int>> result;
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-
-        // Iterate through intervals, considering both start and end times for merging
-        for (int i = 1; i < intervals.size(); i++) {
-            int currentStart = intervals[i][0];
-            int currentEnd = intervals[i][1];
-
-            // Check for overlap or consecutive intervals
-            if (currentStart <= end) {
-                // Update end time to include the larger ending interval
-                end = std::max(end, currentEnd);
-            } else {
-                // No overlap, push the current merged interval and start processing the new one
-                result.push_back({start, end});
-                start = currentStart;
-                end = currentEnd;
             }
         }
-
-        // Push the last merged interval if applicable
-        result.push_back({start, end});
-
-        return result;
+        return ans;
     }
 };

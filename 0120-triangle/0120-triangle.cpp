15ccef1e8f227a5aba3f763ff001177f462d13e9
Kumar Vrchsav <112531483+Vrchsav@users.noreply.github.com>
2025-09-26 01:19:44 +0530

    Time: 0 ms (100%), Space: 11.7 MB (100%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,27 +1,34 @@
 class Solution {
-    int solve(vector<vector<int>>& grid,int i,int j,vector<vector<int>>&dp){
-        if(i==grid.size()-1 && j<grid[i].size()){
-            return grid[i][j];
+    int solve(vector<vector<int>>& grid){
+        int row=grid.size();
+        int col=grid[row-1].size();
+
+        vector<vector<int>>dp(row,vector<int>(row,0));
+
+        for(int i=0;i<col;i++){
+            dp[row-1][i]=grid[row-1][i];
+
         }
-        if(i<0 || j<0){
-            return 1e9;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int down = grid[i][j]+ solve(grid,i+1,j,dp);
-        int diagonal= grid[i][j]+ solve(grid,i+1,j+1,dp);
         
-        return dp[i][j]=min(down,diagonal);
+        for(int i=row-2;i>=0;i--){
+            for(int j=grid[i].size()-1;j>=0;j--){
+                int down=INT_MAX;
+                if(i+1<row)
+         down = grid[i][j]+ dp[i+1][j];
+        int diagonal=INT_MAX;
+        if(i+1<row && j+1<grid[i+1].size())
+         diagonal= grid[i][j]+ dp[i+1][j+1];
+         dp[i][j]=min(down,diagonal);
+
+        }}
+        return dp[0][0];
 
     }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int row=triangle.size();
         if(row==0) return 0;
-        int col=triangle[row-1].size();
-        vector<vector<int>>dp(row,vector<int>(row,-1));
-        return solve(triangle,0,0,dp);
+        return solve(triangle);
         
     }
 };
